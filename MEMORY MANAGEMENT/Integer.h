6006311f67096ffb3f53aaec4a8c3a2022092c22
aridepai17 <advaithdepai26@gmail.com>
2024-07-14 17:43:58 +0530

    Unique Pointer essentials

---
+++ MEMORY MANAGEMENT/Integer.h
@@ -0,0 +1,31 @@
+#pragma once 
+#include <iostream>
+
+class Integer{
+    int *m_Pint;
+public:
+    Integer();
+    Integer(int value);
+    Integer(const Integer &obj);
+    Integer (Integer &&obj);
+    int GetValue() const;
+    void SetValue(int value);
+    ~Integer();
+    Integer & operator ++();
+    Integer operator ++(int);
+    bool operator == (const Integer &a) const;
+
+    //Copy Assignment
+    Integer & operator = (const Integer &a);
+
+    //Move Assignment
+    Integer & operator = (Integer &&a);
+    Integer operator + (const Integer &a) const;
+
+    void operator ()();
+    operator int();
+};
+
+Integer operator +(int x, const Integer &y);
+std::ostream & operator << (std::ostream & out, const Integer & a);
+std::istream & operator >> (std::istream & input, Integer & a);
---
+++ MEMORY MANAGEMENT/uniquepointer.cpp
@@ -0,0 +1,39 @@
+#include "Integer.h"
+#include <memory>
+
+void Display (Integer *p){
+    if (!p){
+        return;
+    }
+    std::cout << p->GetValue() << std::endl;
+}
+
+Integer * GetPointer( int value ){
+    Integer *p = new Integer{ value };
+    return p;
+}
+
+void Store(std::unique_ptr <Integer> &p){
+    std::cout << "Storing data into a file: " << p->GetValue() << std::endl;
+}
+
+void Operate( int value ){
+    std::unique_ptr <Integer> p { GetPointer(value) }; 
+    if ( p == nullptr ){
+        p.reset( new Integer{value} ); //p = new Integer{ value };
+    }
+    p->SetValue(100);
+    Display(p.get());
+    // delete p;
+    //p = nullptr;
+    //p = new Integer{};
+    p.reset( new Integer{value} );
+    *p = __LINE__;
+    Display(p.get());
+    Store(p);
+    //delete p;
+}
+
+int main(){
+    Operate(10);
+}
